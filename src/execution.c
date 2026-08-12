6d222378e26e0193b51c86612eb851c804664d54
Moabed21 <moathabed21@gmail.com>
2026-08-12 16:24:25 +0300

    deep dive into execution flow, by add color function, exit button activated, the main frame buffer

--- src/execution.c
+++ src/execution.c
@@ -6,41 +6,25 @@
 /*   By: moabed <moabed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 12:20:19 by moabed            #+#    #+#             */
-/*   Updated: 2026/08/10 17:28:40 by moabed           ###   ########.fr       */
+/*   Updated: 2026/08/12 16:21:30 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/header.h"
 
-// void	fractal_init(t_fractal *fractal)
-// {
-// 	fractal->mlx_connection = mlx_init();
-// 	data_init(fractal);
-// 	if (fractal->mlx_connection == NULL)
-// 		error();
-// 	fractal->mlx_window = mlx_new_window(fractal->mlx_connection, WIDTH, HEIGHT,
-// 			fractal->name);
-// 	if (fractal->mlx_window == NULL)
-// 	{
-// 		mlx_destroy_display(fractal->mlx_connection);
-// 		free(fractal->mlx_connection);
-// 		error();
-// 	}
-// 	fractal->img.img = mlx_new_image(fractal->mlx_connection, WIDTH, HEIGHT);
-// 	if (fractal->img.img == NULL)
-// 	{
-// 		mlx_destroy_window(fractal->mlx_connection, fractal->mlx_window);
-// 		mlx_destroy_display(fractal->mlx_connection);
-// 		free(fractal->mlx_connection);
-// 		error();
-// 	}
-// 	fractal->img.pixel_ptr = mlx_get_data_addr(fractal->img.img,
-// 			&fractal->img.bits_per_pixel, &fractal->img.line_length,
-// 			&fractal->img.endian);
-// }
+int	get_color(int rgb[3])
+{
+	return ((rgb[0] << 16) | (rgb[1] << 8) | rgb[2]);
+}
 
-// first of all we render the main frame of the window to prevent flickering 
-// if we draw entire 3D frame once
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
 void	mlx(t_cub *cub)
 {
 	cub->mlx_ops.connection = mlx_init();
@@ -49,11 +33,7 @@ void	mlx(t_cub *cub)
 	cub->mlx_ops.window = mlx_new_window(cub->mlx_ops.connection, WIDTH, HEIGHT,
 			"Cub3d");
 	if (!cub->mlx_ops.window)
-	{
-		mlx_destroy_display(cub->mlx_ops.connection);
-		free(cub->mlx_ops.connection);
 		raise_exception(cub, ERR_MLX_WIN);
-	}
 	cub->mlx_ops.img.img_ptr = mlx_new_image(cub->mlx_ops.connection, WIDTH,
 			HEIGHT);
 	if (!cub->mlx_ops.img.img_ptr)
@@ -63,11 +43,25 @@ void	mlx(t_cub *cub)
 			&cub->mlx_ops.img.endian);
 	mlx_put_image_to_window(cub->mlx_ops.connection, cub->mlx_ops.window,
 		cub->mlx_ops.img.img_ptr, 0, 0);
-	mlx_loop(cub->mlx_ops.connection);
+}
+
+int	close_handler(t_cub *cub)
+{
+	free_cub(cub);
+	exit(0);
+	return (0);
 }
 
 void	execution(t_cub *cub)
 {
-	// startin with initiatin mlx first canvas
+	init_player_vectors(cub);
 	mlx(cub);
+	load_images(cub);
+	render_background(cub);
+	// raycast(cub);
+	mlx_put_image_to_window(cub->mlx_ops.connection, cub->mlx_ops.window,
+		cub->mlx_ops.img.img_ptr, 0, 0);
+	mlx_hook(cub->mlx_ops.window, DestroyNotify, StructureNotifyMask,
+		(int (*)())close_handler, cub);
+	mlx_loop(cub->mlx_ops.connection);
 }
